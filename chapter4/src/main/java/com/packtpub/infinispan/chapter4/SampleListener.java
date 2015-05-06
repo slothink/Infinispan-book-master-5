3efb7e5a6cab1ce5aab3b999cba7614b063fb13e
HyunJang Pyun <slothink@gmail.com>
2015-05-06 16:20:06 +0900

    add sample tcp cache

---
+++ chapter4/src/main/java/com/packtpub/infinispan/chapter4/SampleListener.java
@@ -0,0 +1,55 @@
+package com.packtpub.infinispan.chapter4;
+
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryRemoved;
+import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryRemovedEvent;
+import org.infinispan.notifications.cachemanagerlistener.annotation.CacheStarted;
+import org.infinispan.notifications.cachemanagerlistener.annotation.CacheStopped;
+import org.infinispan.notifications.cachemanagerlistener.event.CacheStartedEvent;
+import org.infinispan.notifications.cachemanagerlistener.event.CacheStoppedEvent;
+import org.infinispan.remoting.transport.Address;
+
+/**
+ * Created by slothink on 2015-05-06.
+ */
+@Listener
+public class SampleListener {
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    @CacheEntryCreated
+    public void dataAdded(CacheEntryCreatedEvent event) {
+        if(event.isPre()) {
+            this.print("Going to add new Entry "+event.getKey()+" to the cache");
+        }else {
+            print("Added new entry "+ event.getKey() + " to the cache");
+        }
+    }
+
+    @CacheEntryRemoved
+    public void dataRemoved(CacheEntryRemovedEvent event) {
+        if(event.isPre()) {
+            this.print("Going to remove entry "+event.getKey()+" created in the cache");
+        }else {
+            print("Removed entry "+ event.getKey() + " from the cache");
+        }
+    }
+
+    @CacheStarted
+    public void cacheStarted(CacheStartedEvent event) {
+        System.out.println("My address:"+event.getCacheManager().getAddress());
+        for(Address address: event.getCacheManager().getMembers()) {
+            System.out.println("Member In Cluster:" + address);
+        }
+        print("Cache started");
+    }
+
+    @CacheStopped
+    public void cacheStopped(CacheStoppedEvent event) {
+        print("Cache Stopped");
+    }
+}
---
+++ chapter4/src/main/java/com/packtpub/infinispan/chapter4/SampleTcpCache.java
@@ -0,0 +1,113 @@
+package com.packtpub.infinispan.chapter4;
+
+import model.Ticket;
+import org.infinispan.Cache;
+import org.infinispan.manager.DefaultCacheManager;
+import utils.IOUtils;
+
+import javax.transaction.*;
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by slothink on 2015-05-06.
+ */
+public class SampleTcpCache {
+    DefaultCacheManager m = null;
+    Cache<String, Ticket> cache = null;
+
+    public void start() throws NotSupportedException, SystemException, SecurityException, IllegalStateException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
+
+        try {
+            m = new DefaultCacheManager("sample-tcp.xml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        m.addListener(new SampleListener());
+
+        cache = m.getCache("clusteredCache");
+        cache.addListener(new SampleListener());
+
+
+        String command = ""; // Line read from standard in
+        int ticketid = 1;
+
+        IOUtils.dumpWelcomeMessage();
+
+        while (true){
+            command = IOUtils.readLine("> ");
+            if (command.equals("book")) {
+
+                String name = IOUtils.readLine("Enter name ");
+                String show = IOUtils.readLine("Enter show ");
+
+                Ticket ticket = new Ticket(name,show);
+                cache.put(name, ticket);
+                log("Booked ticket "+ticket);
+                ticketid++;
+            }
+            else if (command.equals("pay")) {
+                String name = IOUtils.readLine("Enter ticketid ");
+                Ticket ticket = cache.remove(name);
+                log("Checked out ticket "+ticket);
+            }
+            else if (command.equals("list")) {
+                Set<String> set = cache.keySet();
+                for (String ticket: set) {
+                    System.out.println(cache.get(ticket));
+                }
+            }
+            else if (command.equals("quit")) {
+                cache.stop();
+                log("Bye");
+                break;
+            }
+            else if (command.equals("change")) {
+                String id = IOUtils.readLine("Enter Ticketid ");
+                String show = IOUtils.readLine("Enter show ");
+                Ticket ticket = cache.get(Integer.parseInt(id));
+                ticket.setShow(show);
+                beginTransaction();
+                cache.put(id, ticket);
+                String captcha =
+                        UUID.randomUUID().toString().substring(0,4);
+                String check =
+                        IOUtils.readLine("Enter captcha "+captcha);
+                if (captcha.equals(check)) {
+                    commitTransaction();
+                    log("Updated ticket "+ticket);
+                }
+                else {
+                    rollbackTransaction();
+                    log("Updated failed!");
+
+                }
+            }
+            else {
+                log("Unknown command "+command);
+                IOUtils.dumpWelcomeMessage();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SecurityException, IllegalStateException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
+        new SampleTcpCache().start();
+
+    }
+    public static void log(String s){
+        System.out.println(s);
+    }
+    public void beginTransaction() throws NotSupportedException, SystemException {
+        cache.getAdvancedCache().
+                getTransactionManager().begin();
+    }
+    public void commitTransaction() throws SecurityException, IllegalStateException, RollbackException, HeuristicMixedException, HeuristicRollbackException, SystemException {
+        cache.getAdvancedCache().
+                getTransactionManager().commit();
+    }
+    public void rollbackTransaction() throws IllegalStateException, SecurityException, SystemException {
+        cache.getAdvancedCache().
+                getTransactionManager().rollback();
+    }
+}
